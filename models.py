9277efd6a177fa986ea3bc994ba46dc21a75eab0
Cleandroh02 <lcesar02@gmail.com>
2023-08-22 22:03:52 -0500

    first commit

---
+++ models.py
@@ -0,0 +1,11 @@
+from typing import Optional
+from pydantic import BaseModel
+
+
+class Book(BaseModel):
+    title: str
+    author: str
+    year: int
+    pages: Optional[int] = None
+    isbn: Optional[str] = None
+    
